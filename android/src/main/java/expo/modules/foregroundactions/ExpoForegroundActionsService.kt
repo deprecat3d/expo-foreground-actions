d571593130d6aae6f3bde135ae8adb8dbaa59743
deprecat3d <raisinbourne@gmail.com>
2025-02-02 05:25:00 -0500

    Update ExpoForegroundActionsService.kt

--- android/src/main/java/expo/modules/foregroundactions/ExpoForegroundActionsService.kt
+++ android/src/main/java/expo/modules/foregroundactions/ExpoForegroundActionsService.kt
@@ -17,6 +17,7 @@ import androidx.core.app.NotificationCompat
 import com.facebook.react.HeadlessJsTaskService
 import com.facebook.react.bridge.Arguments
 import com.facebook.react.jstasks.HeadlessJsTaskConfig
+import androidx.annotation.RequiresApi
 
 class ExpoForegroundActionsService : HeadlessJsTaskService() {
     companion object {
@@ -97,7 +98,7 @@ class ExpoForegroundActionsService : HeadlessJsTaskService() {
         println("Starting foreground")
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            startForeground(notificationId, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC)
+            startForeground(notificationId, notification, getForegroundServiceType())
         } else {
             startForeground(notificationId, notification)
         }
@@ -147,5 +148,10 @@ class ExpoForegroundActionsService : HeadlessJsTaskService() {
 
         stopSelf()  // Always stop the service
         super.onDestroy()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun getForegroundServiceType(): Int {
+        return ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC
     }
 }
