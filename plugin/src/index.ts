ceef38c4770ad176082ea867427f7bc51a878a9a
deprecat3d <raisinbourne@gmail.com>
2025-02-06 01:07:16 -0500

    Create index.ts

---
+++ plugin/src/index.ts
@@ -0,0 +1,10 @@
+import { ConfigPlugin, createRunOncePlugin } from '@expo/config-plugins';
+import { withForegroundActions } from './withForegroundActions';
+
+const withExpoForegroundActions: ConfigPlugin<{ androidNotificationIcon?: string }> = (config, props) => {
+  return withForegroundActions(config, props);
+};
+
+const pkg = require('../../package.json');
+
+export default createRunOncePlugin(withExpoForegroundActions, pkg.name, pkg.version);
